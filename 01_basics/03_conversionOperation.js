2d71f4c323c6a18c17875197e104784ebf203a40
aryasidh <146578432+aryasidh@users.noreply.github.com>
2026-02-02 03:53:36 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);  //number
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NAN (jab bumber me convert nhi hogaa to NAN output degaa)
+//true  => 1; false => 0
+
+let isloggedIn = 1
+
+let booleanIsLoggedin = Boolean(isloggedIn)
+console.log(booleanIsLoggedin);
+
+// 1 => true; 0 => flase
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
