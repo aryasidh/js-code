32def8d651f928e5eff1f4bfb8ccd8426043b4aa
aryasidh <146578432+aryasidh@users.noreply.github.com>
2026-02-02 03:22:18 +0000

    data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all js code as newer version
+
+// alert("hello")  //we are using node js not browser
+
+console.log(3+
+    2)  //yes it will be print But not recomended
+
+    console.log("hitesh");
+
+    //Data Type
+    //number => 2 to power 53
+    //bigint
+    //String => double qutes  ""
+    //boolean => true/false
+    //null =>standlone value
+    //undefined => means value define nhi hui hai
+    //symbol =>finding for unique
+
+
+    //object
+
+    console.log(typeof "hitesg");
+
+
+    console.log(typeof null); //object
+
+    console.log(typeof undefined); //undefined kuki ye ek type hai js ke under
+    
