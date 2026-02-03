198f01ab85a6b4f47a5f54a243e586974c3238cd
aryasidh <146578432+aryasidh@users.noreply.github.com>
2026-02-03 22:59:31 +0000

    LITTLE CHANGE

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,7 +7,7 @@ let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);  //number
 // console.log(valueInNumber);
 
-
+//     STRING TO NUMBER
 //"33" => 33
 //"33abc" => NAN (jab bumber me convert nhi hogaa to NAN output degaa)
 //true  => 1; false => 0
@@ -17,6 +17,8 @@ let isloggedIn = 1
 let booleanIsLoggedin = Boolean(isloggedIn)
 console.log(booleanIsLoggedin);
 
+
+//     NUMBER TO BOOLEAN
 // 1 => true; 0 => flase
 // "" => false
 // "hitesh" => true
