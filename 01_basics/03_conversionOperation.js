c8c2f01140fbeb3ca3b31f314720e89b5b9d76d7
aryasidh <146578432+aryasidh@users.noreply.github.com>
2026-02-04 00:05:36 +0000

    part 02

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isloggedIn = 1
 
 let booleanIsLoggedin = Boolean(isloggedIn)
-console.log(booleanIsLoggedin);
+// console.log(booleanIsLoggedin);
 
 
 //     NUMBER TO BOOLEAN
@@ -27,8 +27,65 @@ console.log(booleanIsLoggedin);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//********************OPERATIONS********************* */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 ki power 3
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " prince"
+
+// let str3 = str1+str2 // String + String → Concatenation
+// console.log(str3);
+
+
+
+// + operator rule:
+// Agar string mil gayi, to + concatenation ban jata hai
+// Execution LEFT to RIGHT hota hai
+
+// console.log("1" + 2);
+// // agar pahla oprant ("1")string hai → dusra oprant number(2) hai then dusra oprent string me convert → result "12"
+
+// console.log(1 + "2");     
+// // number + string → concatenation → result "12"
+
+// console.log("1" + 2 + "2"); 
+// // "1"+2 → "12" (string)
+// // "12"+"2" → "122"
+
+// console.log(1 + 2 + "2");  
+// // 1+2 → 3 (number addition)
+// // 3+"2" → "32"
+
+// console.log(+true);  //not recomended
+// console.log(+"");
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2+2  //not recomended
+
+
+let numb1 = 100;
+ numb1++  //post increment
+ console.log(numb1);
+ 
+ let numb2 = 100
+ ++numb2  //pre increment
+ console.log(numb2);
+
+ 
+ 
 
 
 
