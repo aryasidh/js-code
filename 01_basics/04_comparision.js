72d25e5260637f70e8d699dc759c13d4b1bf2f4c
aryasidh <146578432+aryasidh@users.noreply.github.com>
2026-02-04 00:26:50 +0000

    04_COMPARISION

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+console.log(2>1);  //true
+console.log(2>=2);  //true
+console.log(1<2);  //true
+console.log(2==2);  //true
+console.log(2!=1);  //true
+//above all the comparison is very easy no need to investigate or revision
+
+//comparision se pahle data type should be same
+console.log("2" > 1);  //true
+console.log("02" > 1);  //true
+
+/*-------------------------------------ONLY FOR KNOWLEDGE PRACTICAL KOI USE NHI HAI----------------------------------------- */
+console.log(null > 0);//  false
+// comparison me null number ban jata hai (null → 0)
+// 0 > 0 ❌ false
+
+console.log(null == 0);  //false
+// == null ko sirf undefined ke saath equal maanta hai
+// null == undefined ✔️, but null == 0 ❌
+
+console.log(null >= 0); //true
+// >= comparison me null → 0
+// 0 >= 0 ✔️ true
+
+
+
+
