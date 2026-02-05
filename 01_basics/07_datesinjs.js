8fd86fb60e221490ade50e5fa479d2012064fa24
aryasidh <146578432+aryasidh@users.noreply.github.com>
2026-02-05 04:03:14 +0000

    little update

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -21,9 +21,7 @@ let newDate = new Date()
 // console.log(newDate.getMonth() + 1);  //1 ko es liye add kiya hu kuki js me month to zero se start hota hai na agar add nhi krungaa to user confused ho jayegaa
 // console.log(newDate.getDay())  //sun-0, mon-1, tue-2, wed-3, thus-4, fri-5, sat-6.
 
-newDate.toLocaleString('default', {
-    weekday: "long",
-})
+newDate.toLocaleString('default', {weekday: "long",})  //"long" → Monday, "short" → Mon, "narrow" → M
 
 
 
